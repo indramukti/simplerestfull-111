3b8fedfef3c9e3cbc5063651cebbb5a5691ac83b
indramukti <96394744+indramukti@users.noreply.github.com>
2022-12-05 18:54:59 +0700

    first commit

---
+++ src/test/java/praktikum4/praktikum/PraktikumApplicationTests.java
@@ -0,0 +1,13 @@
+package praktikum4.praktikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
